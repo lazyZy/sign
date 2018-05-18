c046b00cc5c2fe8fda15d8c632ea0eb23a219398
ZY <746234304@qq.com>
2018-05-18 17:35:03 +0800

    远程关联

---
+++ src/test/java/top/mrzhao/SignApplicationTests.java
@@ -0,0 +1,16 @@
+package top.mrzhao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SignApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
